7cb83fb3f058c8f53e2894c6459468e78b9d1d7a
Trung Huỳnh <19520317@ms.uit.edu.vn>
2022-05-13 21:03:54 +0700

    Init project

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+	env: {
+		browser: true,
+		es2021: true,
+	},
+	extends: [
+		'eslint:recommended',
+		'plugin:react/recommended',
+		'plugin:@typescript-eslint/recommended',
+	],
+	parser: '@typescript-eslint/parser',
+	parserOptions: {
+		ecmaFeatures: {
+			jsx: true,
+		},
+		ecmaVersion: 'latest',
+		sourceType: 'module',
+	},
+	plugins: ['react', '@typescript-eslint'],
+	rules: {
+		quotes: ['error', 'single'],
+		semi: ['error', 'always'],
+		'react/jsx-uses-react': 'off',
+		'react/react-in-jsx-scope': 'off',
+		'react/no-unescaped-entities': 0,
+	},
+};
