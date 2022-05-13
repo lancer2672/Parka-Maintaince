15c80ab66d5020c7f467ce48de319924b52d3947
Trung Huỳnh <19520317@ms.uit.edu.vn>
2022-05-14 00:53:03 +0700

    chore: add assets

---
+++ src/assets/images/index.ts
@@ -0,0 +1,7 @@
+const Images = {
+  Card: require("./onboarding/Card.png"),
+  LicensePlate: require("./onboarding/LicensePlate.png"),
+  Parking: require("./onboarding/Parking.png"),
+};
+
+export default Images;
