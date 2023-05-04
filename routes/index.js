bea0c6489f76bdc6f2d9aca9e4d068f88c16e4cc
ngxuantai <xuantai1902@gmail.com>
2023-05-04 22:48:49 +0700

    Api GetAllTicketCompany

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,11 @@ const router = require("express").Router();
 const authRouter = require("./auth.route");
 const userRouter = require("./user.route");
 const companyRouter = require("./company.route");
+const ticketRouter = require("./ticket.route");
 
 router.use("/auth", authRouter);
 router.use("/user", userRouter);
 router.use("/company",companyRouter);
+router.use("/ticket",ticketRouter);
 
 module.exports = router;
