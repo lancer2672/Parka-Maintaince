bed4f478f389efc061294526d754f71b2802de8f
lancer2672 <JBkhanhtran@gmail.com>
2023-05-06 13:19:51 +0700

    create api parking-lot

--- controllers/company.controller.js
+++ controllers/company.controller.js
@@ -10,7 +10,7 @@ exports.CreateCompany = async (req, res) => {
   console.log(email, companyName, phoneNumber, password);
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
-    return res.status(400).json({ errors: errors.array()});
+    return res.status(400).json({ errors: errors.array() });
   }
 
   try {
@@ -19,7 +19,6 @@ exports.CreateCompany = async (req, res) => {
       "INSERT INTO company(name, phone_number, email, password) VALUES ($1, $2, $3, $4) RETURNING *",
       [companyName, phoneNumber, email, passwordHash]
     );
-    console.log("result", result.rows[0]);
     const company = result.rows[0];
     return res.json({
       data: {
@@ -29,7 +28,7 @@ exports.CreateCompany = async (req, res) => {
         name: company.name,
         phoneNumber: company.phone_number,
         email: company.email,
-        password: company.password
+        password: company.password,
       },
     });
   } catch (err) {
@@ -39,32 +38,34 @@ exports.CreateCompany = async (req, res) => {
 };
 
 exports.GetCompanyById = async (req, res) => {
-    const companyId = req.params.id;
-    try {
-      const result = await pool.query("SELECT * FROM company WHERE id = $1", [companyId]);
-      console.log(companyId);
-      if (result.rowCount !== 0) {
-        const company=result.rows[0];
-        return res.json({
-          data: {
-            id: company.id,
-            created_at: company.created_at,
-            updated_at: company.updated_at,
-            name: company.name,
-            phoneNumber: company.phone_number,
-            email: company.email,
-            password: company.password
-          }
-        });
-      } else {
-        return res.status(404).json({ error: {
-            detail: "Wrong id",
-        }});
-
-      }
-    } catch (err) {
-      console.error(err);
-      res.status(500).json({ message: "Internal server error" });
+  const companyId = req.params.id;
+  try {
+    const result = await pool.query("SELECT * FROM company WHERE id = $1", [
+      companyId,
+    ]);
+    console.log(companyId);
+    if (result.rowCount !== 0) {
+      const company = result.rows[0];
+      return res.json({
+        data: {
+          id: company.id,
+          created_at: company.created_at,
+          updated_at: company.updated_at,
+          name: company.name,
+          phoneNumber: company.phone_number,
+          email: company.email,
+          password: company.password,
+        },
+      });
+    } else {
+      return res.status(404).json({
+        error: {
+          detail: "Wrong id",
+        },
+      });
     }
-  };
-  
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
---
+++ routes/parking-lot.route.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const router = express.Router();
+const ParkingLotController = require("./../controllers/parking-lot.controller");
+const verifyToken = require("../middlewares/verifyToken");
+
+router.get("/get-one/:id", ParkingLotController.getOneParkingLot);
+router.get("/get-list", ParkingLotController.getListParkingLot);
+router.delete("/delete/:id", ParkingLotController.deleteOne);
+router.put("/update/:id", ParkingLotController.updateOne);
+router.post("/create/", ParkingLotController.create);
+
+module.exports = router;
